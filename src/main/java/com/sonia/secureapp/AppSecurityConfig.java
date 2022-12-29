97898eaa4ae7460d94d9491045c4c5f66a5abe6e
Kumari Sonia <k.sonia15896@gmail.com>
2022-12-30 01:30:10 +0530

    added username and password from mysql database for spring boot security login

--- src/main/java/com/sonia/secureapp/AppSecurityConfig.java
+++ src/main/java/com/sonia/secureapp/AppSecurityConfig.java
@@ -5,34 +5,44 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig {
+	@Autowired
+	private UserDetailsService userDetailsService;
 
-    @Bean
-   protected UserDetailsService userDetailService() {
-    	Collection<UserDetails> users = new ArrayList<>();
+	@Bean
+	public AuthenticationProvider authProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		return provider;
 
+	}
 
-UserDetails user = User.withDefaultPasswordEncoder()
-.username("user")
-.password("password")
-.roles("USER")
-.build();
-
-users.add(user);
-
-        return new InMemoryUserDetailsManager(users);
-    }
-	
+//	@Bean
+//	protected UserDetailsService userDetailService() {
+//		Collection<UserDetails> users = new ArrayList<>();
+//
+//		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
+//				.build();
+//
+//		users.add(user);
+//
+//		return new InMemoryUserDetailsManager(users);
+//	}
 
 }
---
+++ src/main/java/com/sonia/secureapp/MyUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.sonia.secureapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+	@Autowired
+	private UserRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		User user = repo.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("user not faund");
+		}
+		return new UserPrincipal(user);
+	}
+
+}
